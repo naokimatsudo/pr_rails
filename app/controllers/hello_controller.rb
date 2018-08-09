b43d86851f587683ecbd429952f52ee6bce7f7e1
naokimatsudo <n.m.0201306@docomo.ne.jp>
2018-08-09 12:30:54 +0900

    change controller

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,2 +1,20 @@
 class HelloController < ApplicationController
+
+    def index
+        if params['word'] != nil
+            a = 'Hello, ' + params['word'] + '!'
+        else
+            a = 'this is sample page.'
+        end
+        html = '
+        <html>
+        <body>
+          <h1>Sample Page</h1>
+          <p>' + a + '</p>
+        </body>
+        </html>
+        '
+        render html: html.html_safe
+    end
+
 end
