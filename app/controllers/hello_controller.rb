605ec91f622cdceb5e89c3d7b68b060ba335d0e0
naokimatsudo <n.m.0201306@docomo.ne.jp>
2018-08-09 18:11:55 +0900

    add checkbox

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,13 +3,14 @@ class HelloController < ApplicationController
     def index
       if request.post?
         @title = 'Result'
-        @msg = 'you typed: ' + params['input1'] + '.'
-        @value = params['input1']
+        if params["check1"]
+            @msg = 'you Checked!!'
+        else
+            @msg = 'not checked...'
+        end
       else
         @title = 'Index'
-        @msg = 'type text...'
-        @value = ''
+        @msg = 'check it ...'
       end
-    end
-
+    end 
 end
