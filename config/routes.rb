6ba195ac0071d5343250b7f042138033a301cb18
naokimatsudo <n.m.0201306@docomo.ne.jp>
2018-08-13 13:04:13 +0900

    create message app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
+  get 'dengonban/index'
+  post 'dengonban/index'
+
   get 'hello/index'
   get 'hello', to: 'hello#index'
   post 'hello/index'
